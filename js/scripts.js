208413dd818b0ee0dd16c810f7ca19bc81cb434a
DmitriyShvanyk <soledar1984@gmail.com>
2023-08-15 17:53:00 +0200

    Init

---
+++ js/scripts.js
@@ -0,0 +1,59 @@
+;(function () {
+
+	"use strict";
+
+
+	// load fonst
+    function loadFonst(href) {
+        const head = document.querySelector('head');
+        const link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.href = href;
+        head.appendChild(link)
+    }
+
+    loadFonst('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Montserrat:wght@400;500;600;700&display=swap');
+
+
+
+	// gallery
+	Fancybox.bind("[data-fancybox]", {});
+
+
+	// projects
+	function showMoreProjects() {
+		const projects = document.querySelector('.projects');
+		if(!projects) return;
+
+		const projectsBtnMore = projects.querySelector('.projects-btn-more');
+		const projectsOverlay = projects.querySelector('.projects-overlay');
+
+		projectsBtnMore.addEventListener('click', ()=> {
+			projects.classList.add('is-active');
+			projectsOverlay.classList.add('d-none');
+			projectsBtnMore.classList.add('d-none');
+		})
+	}
+
+	showMoreProjects();
+
+
+	// aos
+	AOS.init({
+		once: true,
+	});
+
+
+	// years
+	function setCurrentYears() {
+		const yearsElems = document.querySelectorAll('.js-years');
+		return yearsElems.forEach(el => el.textContent = new Date().getFullYear());
+	}
+
+
+
+	window.addEventListener('DOMContentLoaded', () => {
+		setCurrentYears();
+	});
+
+})();
